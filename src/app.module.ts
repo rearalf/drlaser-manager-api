ae6118bbe2f0ba34fd1aa59fa553e1f3d4d35644
Ricardo Alf <rear_recinos@hotmail.com>
2025-11-11 09:52:18 -0600

    Add environment configuration and Swagger documentation setup
    - Created .env.template for environment variables
    - Updated package.json and package-lock.json to include @nestjs/config and @nestjs/swagger
    - Configured CORS and global API prefix in main.ts
    - Integrated Swagger for API documentation in development mode

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const allowedOrigins: string = configService.get('ALLOWED_ORIGINS') ?? '*';
+
+  app.enableCors({
+    origin: allowedOrigins === '*' ? true : allowedOrigins.split(','),
+    credentials: true,
+  });
+  app.setGlobalPrefix(`api/${configService.get('API_VERSION') ?? 'v1'}`);
+
+  if (configService.get('NODE_ENV') !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Dr Laser manager API')
+      .setDescription('Documentation about backend')
+      .setVersion(configService.get('BACKEND_VERSION') ?? '0.0.1')
+      .build();
+
+    const documentFactory = (): OpenAPIObject =>
+      SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api-docs', app, documentFactory);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
