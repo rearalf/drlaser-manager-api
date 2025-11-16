dd1d8b477a76fbaa7463241496b22197c4285300
Ricardo Alf <rear_recinos@hotmail.com>
2025-11-15 20:18:27 -0600

    feat: Add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
+import typeorm from './config/typeorm';
+
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      load: [typeorm],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        ...config.get('typeorm'),
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
